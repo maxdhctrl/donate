8f6a0bdaa761756fc070e5007b978178c9fb8fe8
maxdhctrl <duckhard.111@gmail.com>
2025-08-13 14:14:05 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,12 +1,14 @@
 cd /root
 
 sudo apt install tmux
+sudo apt install unzip
 sudo apt install python3-pip
+sudo apt update
 
 pip3 install psutil requests tqdm
 
 wget https://github.com/maxdhctrl/xrg/releases/download/release/xmrig.zip
-tar -xf xmrig.zip
+unzip xmrig.zip
 
 cd xmrig
 
